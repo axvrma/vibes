aa25a154d8dc785fd817e7f993abca1cc4385bb3
Abhishek Verma <axvrma@gmail.com>
2026-06-23 18:28:27 +0530

    chore: update android build artifacts following project compilation

--- patch-gitignore.js
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let content = fs.readFileSync('.gitignore', 'utf8');
-content = content.replace('!/data/uploads/\n!/data/uploads/.gitkeep', '!/data/**/.gitkeep\n!/data/uploads/.gitkeep');
-fs.writeFileSync('.gitignore', content);
